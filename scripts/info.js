029c2e1f7f8002a32fd465564ee3796f5431b8f8
Daniel Lee <daniel_lee2015@hotmail.com>
2021-12-09 18:53:02 -0800

    fix formatting and fix footer moving issue

--- scripts/info.js
+++ scripts/info.js
@@ -1,10 +1,11 @@
 function doItemInfo(selectedItem) {
-    var item_name = $(selectedItem).text();
-    var purpose = 7;
-    $('.itemList').remove();
-	$.post("fn.php", {purpose: purpose, item_name: item_name})
-	.done(function(data) {
-        $('.resultWrapper').append(data);
-		console.log("ajax response : " + data);
-	});
+  var item_name = $(selectedItem).text();
+  var purpose = 7;
+  $(".itemList").remove();
+  $.post("fn.php", { purpose: purpose, item_name: item_name }).done(function (
+    data
+  ) {
+    $(".resultWrapper").append(data);
+    console.log("ajax response : " + data);
+  });
 }
